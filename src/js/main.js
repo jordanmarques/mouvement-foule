06939261b26bdcdfa10fc3e6480171c70a340858
Jordan Marques <jordanmarques1994@gmail.com>
2015-11-11 15:30:28 +0100

    display map

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
 const ts = new Tileset("tiles.png");
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var textMap;
 const FLOOR = 1;
 const WALL = 2;
 const GRASS = 3;
@@ -9,49 +12,68 @@ const TILES_SIZE = 30;
 
 
 window.onload = function() {
-        var canvas = document.getElementById('canvas');
         var context = canvas.getContext('2d');
         window.addEventListener('resize', resizeCanvas, false);
 
         function resizeCanvas() {
             canvas.width = window.innerWidth;
             canvas.height = window.innerHeight;
-            draw();
-        }
-        function draw() {
-            var ctx = canvas.getContext('2d');
-            ts.drawTile(FLOOR, ctx, 10, 40);
-            ts.drawTile(WALL, ctx, 10, 10);
-            ts.drawTile(GRASS, ctx, 40, 10);
-            ts.drawTile(DOOR, ctx, 130, 10);
-            ts.drawTile(MOUSE, ctx, 170, 10);
-            ts.drawTile(CHEESE, ctx, 210, 10);
+            if(textMap){
+                draw(textMap);
+            }
         }
         resizeCanvas();
 };
 
 document.getElementById('file').onchange = function() {
     $.get('maps/'+$('#file').val().replace("C:\\fakepath\\", ""), function(data) {
-        console.log(data);
-
-        for(i; i<data.length; i++) {
-            switch(data.charAt(i)) {
-                case '*' :
-                    break;
-
-                case 'G' :
-                    break;
-
-                case ' ' :
-                    break;
-
-                case 'D' :
-                    break;
-
-                case 'A' :
-                    break;
-            }
-        }
-
-    })
+         textMap = data;
+        draw(textMap);
+    });
 };
+
+function draw(data) {
+    var x = 10;
+    var y  = 10;
+    for (var i = 0; i < data.length; i++) {
+        switch (data.charAt(i)) {
+            case '*' :
+                ts.drawTile(WALL, ctx, x, y);
+                x += TILES_SIZE;
+                break;
+
+            case 'G' :
+                ts.drawTile(GRASS, ctx, x, y);
+                x += TILES_SIZE;
+                break;
+
+            case ' ' :
+                ts.drawTile(FLOOR, ctx, x, y);
+                x += TILES_SIZE;
+                break;
+
+            case 'D' :
+                ts.drawTile(DOOR, ctx, x, y);
+                x += TILES_SIZE;
+                break;
+
+            case 'A' :
+                ts.drawTile(CHEESE, ctx, x, y);
+                x += TILES_SIZE;
+                break;
+
+            case '\n' :
+                x = 10;
+                y += TILES_SIZE;
+
+            case '\r' :
+                break;
+            default :
+
+                ts.drawTile(FLOOR, ctx, x, y);
+                x += TILES_SIZE;
+                break;
+
+        }
+    }
+}
