4469b77bf71dcf2d9cd4eaa9d069bdb7ec9bab1b
Jordan Marques <jordanmarques1994@gmail.com>
2015-11-11 12:03:50 +0100

    revert angular and set canvas resizable

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,31 @@
-var ts = new Tileset("basique2.png");
+var ts = new Tileset("tiles.png");
 
 window.onload = function() {
-	var canvas = document.getElementById('canvas');
-	var ctx = canvas.getContext('2d');
-    ts.dessinerTile(1, ctx, 10, 10);
-    ts.dessinerTile(2, ctx, 50, 10);
-    ts.dessinerTile(3, ctx, 90, 10);
-    ts.dessinerTile(4, ctx, 130, 10);
-    ts.dessinerTile(5, ctx, 170, 10);
 
-}
+    (function() {
+        var canvas = document.getElementById('canvas'),
+            context = canvas.getContext('2d');
+
+        window.addEventListener('resize', resizeCanvas, false);
+
+        function resizeCanvas() {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+
+            drawStuff();
+        }
+        resizeCanvas();
+
+        function drawStuff() {
+            var ctx = canvas.getContext('2d');
+            ts.dessinerTile(1, ctx, 10, 10);
+            ts.dessinerTile(2, ctx, 50, 10);
+            ts.dessinerTile(3, ctx, 90, 10);
+            ts.dessinerTile(4, ctx, 130, 10);
+            ts.dessinerTile(5, ctx, 170, 10);
+        }
+    })();
+
+
+
+};
