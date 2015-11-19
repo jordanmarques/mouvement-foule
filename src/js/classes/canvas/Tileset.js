bd9fe9f7711986ff42bdcfe626f20a67dfe7c53d
Jordan Marques <jordanmarques1994@gmail.com>
2015-11-19 17:40:55 +0100

    suppress last comment

--- src/js/classes/canvas/Tileset.js
+++ src/js/classes/canvas/Tileset.js
@@ -1,19 +1,13 @@
 const TILES_SIZE_FULL = 22;
 function Tileset(url) {
-	// Chargement de l'image dans l'attribut image
 	this.image = new Image();
 	this.image.referenceDuTileset = this;
 	this.image.onload = function() {
-		if(!this.complete) 
-			throw new Error("Erreur de chargement du tileset nommé \"" + url + "\".");
-		
-		// Largeur du tileset en tiles
 		this.referenceDuTileset.largeur = this.width / TILES_SIZE_FULL;
 	};
 	this.image.src = "tilesets/" + url;
 }
 
-// Méthode de dessin du tile numéro "numero" dans le contexte 2D "context" aux coordonnées x et y
 Tileset.prototype.drawTile = function(numero, context, xDestination, yDestination) {
 	var xSourceEnTiles = numero % this.largeur;
 	if(xSourceEnTiles == 0) xSourceEnTiles = this.largeur;
